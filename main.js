25c16cade8e60a7676df5b2ed61751b636afa74a
Camron Withrow <cmwithrow9@gmail.com>
2023-04-18 14:28:39 -0400

    added dynamic nav menu updates

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const body = document.querySelector("body");
-const mobileNavigation = document.querySelector("nav");
+const navigation = document.querySelector("nav");
 const openMenuButton = document.querySelector(".open-menu");
 const closeMenuButton = document.querySelector(".close-menu");
 
 openMenuButton.addEventListener("click", () => {
-    mobileNavigation.dataset.isHidden = false;
+    navigation.dataset.isHidden = false;
     body.classList.add("no-scroll");
 })
 
 closeMenuButton.addEventListener("click", () => {
-    mobileNavigation.dataset.isHidden = true;
+    navigation.dataset.isHidden = true;
     body.classList.remove("no-scroll");
 })
+
+const mediaQuery = window.matchMedia('(max-width: 50em)');
+
+mediaQuery.addEventListener("change", () => {
+    if (mediaQuery.matches) {
+        navigation.dataset.isHidden = true;
+    } else {
+        navigation.dataset.isHidden = false;
+    }
+});
